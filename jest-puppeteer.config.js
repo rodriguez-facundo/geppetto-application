cb62f8a2be916758583cbf723edb29c08cdc8b9e
rodriguez-facundo <bruseghini.f@gmail.com>
2019-06-15 21:44:41 -0300

    wip

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,19 +1,22 @@
 module.exports = { 
   launch: { 
-    headless: true,
-    args: [
-      '--headless',
-      '--no-zygote',
-      '--use-gl=swiftshader',
-      '--enable-webgl',
-      '--hide-scrollbars',
-      '--mute-audio',
-      '--no-first-run',
-      '--proxy-server="direct://"',
-      '--proxy-bypass-list=*',
-      '--window-size=1280,1024',
-      '--user-data-dir=./chromeData',
-      '--no-sandbox',
-    ]
+    headless: false,
+  
   }, 
 }
+
+
+// args: [
+//   '--headless',
+//   '--no-zygote',
+//   '--use-gl=swiftshader',
+//   '--enable-webgl',
+//   '--hide-scrollbars',
+//   '--mute-audio',
+//   '--no-first-run',
+//   '--proxy-server="direct://"',
+//   '--proxy-bypass-list=*',
+//   '--window-size=1280,1024',
+//   '--user-data-dir=./chromeData',
+//   '--no-sandbox',
+// ]
